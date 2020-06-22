da0c4a5c38c8de969149520708691735c500e1ac
Isabela Rosolen <rosolen@work.co>
2020-06-22 16:00:35 -0300

    Start to do List

--- index.js
+++ index.js
@@ -1,8 +1,38 @@
-const menuButton = document.querySelector("[data-menu-button]");
-const menu = document.querySelector("[data-menu]");
 
-const onMenuButtonClick = () => {
-    menu.classList.add("menu-open");
+const menuOpenButton = document.querySelector("[data-menu-open-button]");
+const menu = document.querySelector("[data-menu]");
+const menuCloseButton = menu.querySelector("[data-menu-close-button]");
+
+const openCloseMenu = () => {
+    menu.classList.toggle("menu-open")
 };
 
-menuButton.addEventListener('click', onMenuButtonClick);
+// const closeMenu = () => {
+//     menu.classList.remove("menu-open")
+// };
+
+menuOpenButton.addEventListener("click", openCloseMenu);
+menuCloseButton.addEventListener("click", closeMenu);
+
+
+//ordem: a ordem nao faz diferença se eu estiver usando o dom / se eu especifico o PAI, ai ele precisa estar na ordem
+//agora pensar na interaçaos - que será o click no botão
+//nossa função - a gente que vai criar a propria funçao
+
+//nomear a minha funçao
+
+// --
+
+// const menuButton = document.querySelector("[data-menu-button]"); -- o document é o dom (objeto), (Método) query selector é o que busca
+// const menu = document.querySelector("[data-menu]");
+
+// const onMenuButtonClick = () => {
+//     menu.classList.add("menu-open");
+// };
+
+// const onMenuButtonClick = () => {
+//     menu.classList.add("menu-close");
+//     menu.classList.remove("menu-open");
+// }
+
+// menuButton.addEventListener('click', onMenuButtonClick);
